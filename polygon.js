fb9c2881ac2f05cc65d50ed75ac1b4caf143cc34
scottkiersten-hash <scottkiersten@gmail.com>
2026-03-09 12:56:50 +0700

    Update polygon.js

--- polygon.js
+++ polygon.js
@@ -1,1 +1,34 @@
+const { ethers } = require("ethers");
 
+const provider = new ethers.JsonRpcProvider(process.env.POLYGON_RPC_URL);
+
+const wallet = new ethers.Wallet(
+  process.env.POLYGON_PRIVATE_KEY,
+  provider
+);
+
+async function anchorHashToPolygon(hash) {
+
+  try {
+
+    const tx = await wallet.sendTransaction({
+      to: wallet.address,
+      value: 0,
+      data: ethers.hexlify(ethers.toUtf8Bytes(hash))
+    });
+
+    console.log("Polygon TX:", tx.hash);
+
+    return tx.hash;
+
+  } catch (error) {
+
+    console.error("Polygon anchor error:", error);
+
+    return null;
+
+  }
+
+}
+
+module.exports = { anchorHashToPolygon };
