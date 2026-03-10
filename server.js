e19dae52b1b4b612c031bc5cf38daba987d80d8d
scottkiersten-hash <scottkiersten@gmail.com>
2026-03-10 15:40:31 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
-
+import { queueHash, processBatch } from "./merkleAnchor.js";
 import anchorToPolygon from "./polygon.js";
 import express from "express";
 import cors from "cors";
